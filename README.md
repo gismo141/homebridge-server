3a2307a3a93c675a8204ab71daa67191ad8ab725
gismo141 <michael.r141@gmail.com>
2016-04-17 18:51:44 +0200

    Basic working server

---
+++ README.md
@@ -0,0 +1,61 @@
+# Homebridge Server - a plugin to configure your homebridge-devices via your Browser
+
+The purpose of this plugin is to change your homebridge configuration via the webbrowser.
+Therefore the JSON-things will be handled by the plugin and you - as a user - can easily add your devices to the bridge.
+
+![Overview](overview.png)
+
+## What will you do with this plugin?
+
+1. Change the broadcasted name of your Homebridge
+2. Change the MAC-address used to identify your Homebridge
+3. Change the PIN to verify your Homebridge
+4. Add or Remove platforms
+5. Add or Remove accessories
+6. Change the names of your services
+
+## How to setup?
+
+Clone this repository (to e.g.: `~/Developer/homebridge-server`)
+
+```Bash
+git clone https://github.com/gismo141/homebridge-server ~/Developer/homebridge-server
+```
+
+Add the following platform to your Homebridge `config.json`:
+```JSON
+{
+	"platform": "Server",
+	"name": "Homebridge Server for browser-based Configuration"
+}
+```
+
+## How to use it?
+
+Depending on your configuration you need to adapt the call!
+The basic structure is:
+
+```Bash
+homebridge -D -U <HOMEBRIDGE_CONFIG_FOLDER> -P <LOCATION_OF_CLONED_SERVER>
+```
+
+so using the above example:
+
+```Bash
+homebridge -D -U ~/.homebridge -P ~/Developer/homebridge-server
+```
+
+After this command, you should be able to access the server on your local machine under `http://localhost:8765`.
+
+## How could you contribute?
+
+1. [Fork this project][fork] to your account.
+2. [Create a new branch][branch] for the improvements, you intend to make.
+3. **Make the changements in your fork.**
+4. [Send a pull-request][pr] from your fork’s branch to my `master` branch.
+ 
+You can always use the web-interface to make the changes you want. It helps you automizing the workflow from above.
+
+[fork]: http://help.github.com/forking/
+[branch]: https://help.github.com/articles/creating-and-deleting-branches-within-your-repository
+[pr]: http://help.github.com/pull-requests/
--- index.js
+++ index.js
@@ -1,294 +1,122 @@
-var http = require('http');
-var Accessory, Service, Characteristic, UUIDGen;
+var Service, Characteristic, LastUpdate;
 
 module.exports = function(homebridge) {
-  console.log("homebridge API version: " + homebridge.version);
-
-  // Accessory must be created from PlatformAccessory Constructor
-  Accessory = homebridge.platformAccessory;
-
-  // Service and Characteristic are from hap-nodejs
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  UUIDGen = homebridge.hap.uuid;
-  
-  // For platform plugin to be considered as dynamic platform plugin,
-  // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
-  homebridge.registerPlatform("homebridge-server", "Server", Server, true);
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerPlatform("homebridge-server", "Server", Server);
 }
 
-// Platform constructor
-// config may be null
-// api may be null if launched from old homebridge version
-function Server(log, config, api) {
-  console.log("Server Init");
-  this.log = log;
-  this.config = config;
-  this.accessories = [];
+function Server(log, config) {
+    var self = this;
+    self.config = config;
+    self.log = log;
+    var http = require("http");
 
-  this.requestServer = http.createServer(function(request, response) {
-    if (request.url === "/add") {
-      this.addAccessory();
-      response.writeHead(204);
-      response.end();
+    var configJSON = require(process.argv[process.argv.indexOf('-U') + 1] + '/config.json');
+    var platformsJSON = configJSON.platforms;
+    var platforms = "";
+    var accessoriesJSON = configJSON.accessories;
+    var accessories = "";
+
+    var bootstrap = "<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css'>";
+    var font = "<link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>";
+    var style = "<style>h1, h2, h3, h4, h5, h6 {font-family: 'Open Sans', sans-serif;}p, div {font-family: 'Open Sans', sans-serif;} input[type='radio'], input[type='checkbox'] {line-height: normal; margin: 0;}</style>"
+    var header = "<html><meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'><head><title>Homebridge - Configuration</title>" + bootstrap + font + style + "</head>"; 
+    var footer = "<script defer='defer' src='//maxcdn.bootstrapcdn.com/bootstrap/latest/js/bootstrap.min.js'></script></body></html>";
+
+    var table1 =  (function () {/* 
+    <div class="table-responsive"> 
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Type</th>
+            <th>Name</th>
+            <th>Info</th>
+          </tr>
+        </thead>
+        <tbody>
+    */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+
+    var table2 =  (function () {/*  
+        </tbody>
+      </table>
+    </div>
+    */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+
+    for(var platform in platformsJSON){
+        platforms = platforms + "<tr><td><div class='checkbox'><label><input type='checkbox' value=''></label></div></td><td>";
+	platforms = platforms + platformsJSON[platform].platform + "</td><td>";
+	platforms = platforms + platformsJSON[platform].name + "</td><td>";
+        var tempArray = [];
+        for(var element_accessory in accessory){
+          tempArray.push(element_accessory);
+          tempArray.push(accessory[element_accessory] + '<br>');
+        }
+        tempArray.splice(tempArray.indexOf('name'), 2);
+        tempArray.splice(tempArray.indexOf('accessory'), 2);
+        platforms = platforms + tempArray + "</td></tr>";
     }
 
-    if (request.url == "/reachability") {
-      this.updateAccessoriesReachability();
-      response.writeHead(204);
-      response.end();
+    for(var id_accessory in accessoriesJSON){
+        var accessory = accessoriesJSON[id_accessory];
+        accessories = accessories + "<tr><td><div class='checkbox'><label><input type='checkbox' value=''></label></div></td><td>";
+        accessories = accessories + accessory.accessory + "</td><td>";
+        accessories = accessories + accessory.name + "</td><td>";
+        var tempArray = [];
+        for(var element_accessory in accessory){
+          tempArray.push(element_accessory);
+          tempArray.push(accessory[element_accessory] + '<br>');
+        }
+        tempArray.splice(tempArray.indexOf('name'), 2);
+        tempArray.splice(tempArray.indexOf('accessory'), 2);
+        accessories = accessories + tempArray + "</td></tr>";
     }
 
-    if (request.url == "/remove") {
-      this.removeAccessory();
-      response.writeHead(204);
+    var bridgeName = "<div class='form-group'><label for='homebridgename'>Name:</label><input type='text' class='form-control' id='homebridgename' value='" + configJSON.bridge.name + "'></div>";
+    var bridgeUsername = "<div class='form-group'><label for='username'>Username:</label><input type='text' class='form-control' id='username' value='" + configJSON.bridge.username + "'></div>";
+    var bridgePin = "<div class='form-group'><label for='pin'>Pin:</label><input type='text' class='form-control' id='pin' value='" + configJSON.bridge.pin + "'></div>";
+
+    var server = http.createServer(function(request, response) {
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(header);
+      response.write("<div class='container'>");
+      response.write("<body><h1>Homebridge</h1>");
+
+      response.write("<h2>Configuration</h2>");
+      response.write(bridgeName + bridgeUsername + bridgePin);
+
+      response.write("<h2>Platforms</h2>");
+      if(JSON.stringify(platformsJSON) != "[]") {
+        response.write(table1 + platforms + table2);
+      } else {
+        response.write("No platforms installed or configured!");
+      }
+
+      response.write("<h2>Accessories</h2>");
+      if(JSON.stringify(accessoriesJSON) != "[]") {
+        response.write(table1 + accessories + table2);
+      } else {
+        response.write("No accessories installed or configured!");
+      }
+
+      response.write("<div class='block-center'>");
+      response.write("<div class='btn-group'>");
+      response.write("<input id='add' name='add' type='submit' class='btn btn-default' value='Add Accessory'>");
+      response.write("<input id='Remove' name='Remove' type='submit' class='btn btn-default' value='Remove Accessory' width='87px !important'>");
+      response.write("</div>");
+      response.write("</div>");
+
+      response.write("</div>");
+      response.write(footer);
       response.end();
-    }
-  }.bind(this));
-
-  this.requestServer.listen(18081, function() {
-    console.log("Server Listening...");
-  });
-
-  if (api) {
-      // Save the API object as plugin needs to register new accessory via this object.
-      this.api = api;
-
-      // Listen to event "didFinishLaunching", this means homebridge already finished loading cached accessories
-      // Platform Plugin should only register new accessory that doesn't exist in homebridge after this event.
-      // Or start discover new accessories
-      this.api.on('didFinishLaunching', function() {
-        console.log("Plugin - DidFinishLaunching");
-      }.bind(this));
-  }
-
-var configJSON = require('/Users/michaelriedel/.homebridge/config.json');
-var platformsJSON = configJSON.platforms;
-var platforms = "";
-var accessoriesJSON = configJSON.accessories;
-var accessories = "";
-
-var bootstrap = "<link rel=\"stylesheet\" href=\"//maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css\">";
-var font = "<link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>";
-var style = "<style>h1, h2, h3, h4, h5, h6 {font-family: 'Open Sans', sans-serif;}p, div {font-family: 'Open Sans', sans-serif;}</style>"
-var header = "<html><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no\"><head><title>Homebridge - Configuration</title>" + bootstrap + font + style + "</head>"; 
-var footer = "<script defer=\"defer\" src=\"//maxcdn.bootstrapcdn.com/bootstrap/latest/js/bootstrap.min.js\"></script></body></html>";
-
-var table1 =  (function () {/* 
-<div class="table-responsive"> 
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Type</th>
-        <th>Name</th>
-        <th>Info</th>
-      </tr>
-    </thead>
-    <tbody>
-*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
-
-var table2 =  (function () {/*  
-    </tbody>
-  </table>
-</div>
-*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
-
-for(var platform in platformsJSON){
-    platforms = platforms + "<tr><td>" + platformsJSON[platform].platform + "</td><td>" + platformsJSON[platform].name + "</td></tr>";
-}
-
-for(var accessory in accessoriesJSON){
-    accessories = accessories + "<tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>" + accessoriesJSON[accessory].accessory + "</td><td>" + accessoriesJSON[accessory].name + "</td><td>" + accessoriesJSON[accessory].name + "</td></tr>";
-}
-
-var bridgeName = "<div class=\"form-group\"><label for=\"homebridgename\">Name:</label><input type=\"text\" class=\"form-control\" id=\"homebridgename\" value=\"" + configJSON.bridge.name + "\"></div>";
-var bridgeUsername = "<div class=\"form-group\"><label for=\"username\">Username:</label><input type=\"text\" class=\"form-control\" id=\"username\" value=\"" + configJSON.bridge.username + "\"></div>";
-var bridgePin = "<div class=\"form-group\"><label for=\"pin\">Pin:</label><input type=\"text\" class=\"form-control\" id=\"pin\" value=\"" + configJSON.bridge.pin + "\"></div>";
-
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(header);
-  response.write("<div class=\"container\">");
-  response.write("<body><h1>Homebridge</h1>");
-
-  response.write("<h2>Configuration</h2>");
-  response.write(bridgeName + bridgeUsername + bridgePin);
-
-  response.write("<h2>Platforms</h2>");
-  if(JSON.stringify(platformsJSON) != "[]") {
-    response.write(table1 + platforms + table2);
-  } else {
-    response.write("No platforms installed or configured!")
-  }
-
-  response.write("<h2>Accessories</h2>");
-  if(JSON.stringify(accessoriesJSON) != "[]") {
-    response.write(table1 + accessories + table2);
-  } else {
-    response.write("No accessories installed or configured!")
-  }
-
-  response.write("</div>")
-  response.write(footer);
-  response.end();
-});
-
-server.listen(8765);
-console.log("Server is listening");
-}
-
-// Function invoked when homebridge tries to restore cached accessory
-// Developer can configure accessory at here (like setup event handler)
-// Update current value
-Server.prototype.configureAccessory = function(accessory) {
-  console.log("Plugin - Configure Accessory: " + accessory.displayName);
-
-  // set the accessory to reachable if plugin can currently process the accessory
-  // otherwise set to false and update the reachability later by invoking 
-  // accessory.updateReachability()
-  accessory.reachable = true;
-
-  accessory.on('identify', function(paired, callback) {
-    console.log("Identify!!!");
-    callback();
-  });
-
-  if (accessory.getService(Service.Lightbulb)) {
-    accessory.getService(Service.Lightbulb)
-    .getCharacteristic(Characteristic.On)
-    .on('set', function(value, callback) {
-      console.log("Light -> " + value);
-      callback();
     });
-  }
 
-  this.accessories.push(accessory);
+    server.listen(8765);
+    console.log("Server is listening");
 }
-
-//Handler will be invoked when user try to config your plugin
-//Callback can be cached and invoke when nessary
-Server.prototype.configurationRequestHandler = function(context, request, callback) {
-  console.log("Context: ", JSON.stringify(context));
-  console.log("Request: ", JSON.stringify(request));
-
-  // Check the request response
-  if (request && request.response && request.response.inputs && request.response.inputs.name) {
-    this.addAccessory(request.response.inputs.name);
-
-    // Invoke callback with config will let homebridge save the new config into config.json
-    // Callback = function(response, type, replace, config)
-    // set "type" to platform if the plugin is trying to modify platforms section
-    // set "replace" to true will let homebridge replace existing config in config.json
-    // "config" is the data platform trying to save
-    callback(null, "platform", true, {"platform":"Server", "otherConfig":"SomeData"});
-    return;
-  }
-
-  // - UI Type: Input
-  // Can be used to request input from user
-  // User response can be retrieved from request.response.inputs next time
-  // when configurationRequestHandler being invoked
-
-  var respDict = {
-    "type": "Interface",
-    "interface": "input",
-    "title": "Add Accessory",
-    "items": [
-      {
-        "id": "name",
-        "title": "Name",
-        "placeholder": "Fancy Light"
-      }//, 
-      // {
-      //   "id": "pw",
-      //   "title": "Password",
-      //   "secure": true
-      // }
-    ]
-  }
-
-  // - UI Type: List
-  // Can be used to ask user to select something from the list
-  // User response can be retrieved from request.response.selections next time
-  // when configurationRequestHandler being invoked
-
-  // var respDict = {
-  //   "type": "Interface",
-  //   "interface": "list",
-  //   "title": "Select Something",
-  //   "allowMultipleSelection": true,
-  //   "items": [
-  //     "A","B","C"
-  //   ]
-  // }
-
-  // - UI Type: Instruction
-  // Can be used to ask user to do something (other than text input)
-  // Hero image is base64 encoded image data. Not really sure the maximum length HomeKit allows.
-
-  // var respDict = {
-  //   "type": "Interface",
-  //   "interface": "instruction",
-  //   "title": "Almost There",
-  //   "detail": "Please press the button on the bridge to finish the setup.",
-  //   "heroImage": "base64 image data",
-  //   "showActivityIndicator": true,
-  // "showNextButton": true,
-  // "buttonText": "Login in browser",
-  // "actionURL": "https://google.com"
-  // }
-
-  // Plugin can set context to allow it track setup process
-  context.ts = "Hello";
-
-  //invoke callback to update setup UI
-  callback(respDict);
-}
-
-// Sample function to show how developer can add accessory dynamically from outside event
-Server.prototype.addAccessory = function(accessoryName) {
-  console.log("Add Accessory");
-  var uuid;
-
-  if (!accessoryName) {
-    accessoryName = "Test Accessory"
-  }
-
-  uuid = UUIDGen.generate(accessoryName);
-
-  var newAccessory = new Accessory(accessoryName, uuid);
-  newAccessory.on('identify', function(paired, callback) {
-    console.log("Identify!!!");
-    callback();
-  });
-  // Plugin can save context on accessory
-  // To help restore accessory in configureAccessory()
-  // newAccessory.context.something = "Something"
-
-  newAccessory.addService(Service.Lightbulb, "Test Light")
-  .getCharacteristic(Characteristic.On)
-  .on('set', function(value, callback) {
-    console.log("Light -> " + value);
-    callback();
-  });
-
-  this.accessories.push(newAccessory);
-  this.api.registerPlatformAccessories("homebridge-server", "Server", [newAccessory]);
-}
-
-Server.prototype.updateAccessoriesReachability = function() {
-  console.log("Update Reachability");
-  for (var index in this.accessories) {
-    var accessory = this.accessories[index];
-    accessory.updateReachability(false);
-  }
-}
-
-// Sample function to show how developer can remove accessory dynamically from outside event
-Server.prototype.removeAccessory = function() {
-  console.log("Remove Accessory");
-  this.api.unregisterPlatformAccessories("homebridge-server", "Server", this.accessories);
-
-  this.accessories = [];
+Server.prototype.accessories = function(callback) {
+    var self = this;
+    self.accessories = [];
+    callback(self.accessories);
 }
