0aada8baa74f2fabdb9993d840e1bc7e03e9389e
gismo141 <michael.r141@gmail.com>
2016-11-22 23:41:54 +0100

    Added ability to add accessories and platforms, Fixes #1

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function Server(log, config) {
     var http = require('http');
 
     // ... extract the platforms JSON-object and instantiate string value ...
-    var platformsJSON = "";
+    var platformsJSON = {};
     var platforms = "";
     // ... extract the accessories JSON-object and instantiate string value...
-    var accessoriesJSON = "";
+    var accessoriesJSON = {};
     var accessories = "";
 
     // Prepare cosmetics for the site
@@ -26,23 +26,22 @@ function Server(log, config) {
     // - fontface with Apple-like Open Sans
     // - general header with responsive design
     // - JS with Twitter bootstrap
-    var bootstrap = "<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css'>";
+    var bootstrap = "<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css'>"
+    /* + "<link href='//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css' rel='stylesheet'/>"*/;
     var font = "<link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>";
     var style = "<style>h1, h2, h3, h4, h5, h6 {font-family: 'Open Sans', sans-serif;}p, div {font-family: 'Open Sans', sans-serif;} input[type='radio'], input[type='checkbox'] {line-height: normal; margin: 0;}</style>"
     var header = "<html><meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'><head><title>Homebridge - Configuration</title>" + bootstrap + font + style + "</head>";
-    var footer = "<script defer='defer' src='//maxcdn.bootstrapcdn.com/bootstrap/latest/js/bootstrap.min.js'></script></body></html>";
+    var footer = "<script defer='defer' src='//maxcdn.bootstrapcdn.com/bootstrap/latest/js/bootstrap.min.js'></script></body></html>"
+    /* + "<script src='//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js'></script>"
+     + "<script> $(document).ready(function() { $.fn.editable.defaults.mode = 'popup';  $('#username').editable(); }); </script>"*/;
 
-    // Prepare the html-markup as string for the presented tables
-    // - tables are responsive and glow when hovered
-    // - present Type, Name and additional Info
-    // - content is added later by concatenating table1 + 'content' + table2
     var table1 = (function() {/* 
             <div class="table-responsive"> 
               <table class="table table-hover">
                 <thead>
                   <tr>
-                    <th width='10%'>Type</th>
-                    <th width='40%'>Name</th>
+                    <th width='15%'>Type</th>
+                    <th width='35%'>Name</th>
                     <th width='40%'>Info</th>
                     <th width='10%'></th>
                   </tr>
@@ -79,7 +78,7 @@ function Server(log, config) {
                 if (req.method == 'POST') {
 
                     req.on('data', function(chunk) {
-                        receivedData = chunk.toString();
+                        var receivedData = chunk.toString();
                         console.log("[Homebridge-Server] received body data: " + receivedData);
                         var arr = receivedData.split("&");
                         configJSON.bridge.name = arr[0].replace('bridgeName=','');
@@ -96,14 +95,44 @@ function Server(log, config) {
 
                 break;
             case '/addPlatform':
-                printAddPlatformPage(res);
+                printAddPage(res, "Platform");
                 break;
             case '/addAccessory':
-                printAddAccessoryPage(res);
+                printAddPage(res, "Accessory");
                 break;
             case '/savePlatformSettings':
+                if (req.method == 'POST') {
+                    req.on('data', function(chunk) {
+                        var receivedData = decypherText(chunk).replace('PlatformToAdd=','');
+                            configJSON.platforms.push(JSON.parse(receivedData));
+                            if(configJSON.platforms.length == 1) {
+                                configJSON.platforms = JSON.parse(JSON.stringify(configJSON.platforms).replace('[,','['));
+                            }
+                            saveConfig(res);
+                            console.log("[Homebridge-Server] Saved platform " + JSON.parse(receivedData).name + ".");
+                    });
+                    req.on('end', function(chunk) { });
+
+                } else {
+                    console.log("[Homebridge-Server] [405] " + req.method + " to " + req.url);
+                }
                 break;
             case '/saveAccessorySettings':
+                if (req.method == 'POST') {
+                    req.on('data', function(chunk) {
+                        var receivedData = decypherText(chunk).replace('AccessoryToAdd=','');
+                            configJSON.accessories.push(JSON.parse(receivedData));
+                            if(configJSON.accessories.length == 1) {
+                                configJSON.accessories = JSON.parse(JSON.stringify(configJSON.accessories).replace('[,','['));
+                            }
+                            saveConfig(res);
+                            console.log("[Homebridge-Server] Saved accessory " + JSON.parse(receivedData).name + ".");
+                    });
+                    req.on('end', function(chunk) { });
+
+                } else {
+                    console.log("[Homebridge-Server] [405] " + req.method + " to " + req.url);
+                }
                 break;
             default:
                 url = req.url;
@@ -112,109 +141,111 @@ function Server(log, config) {
                     if (object.indexOf('Platform') !== -1) {
                         platform = object.replace('Platform', '');
                         delete configJSON.platforms[platform];
-                        platformsJSON = configJSON.platforms;
+                        console.log("[Homebridge-Server] Removed platform " + platform + ".");
                     } else if (object.indexOf('Accessory') !== -1) {
                         accessory = object.replace('Accessory', '');
                         delete configJSON.accessories[accessory];
-                        accessoriesJSON = configJSON.accessories;
+                        console.log("[Homebridge-Server] Removed accessory " + accessory + ".");
                     }
                     saveConfig(res);
-                    console.log("[Homebridge-Server] Saved platform and accessory settings.");
                 }
         };
     }
 
-    function prepareConfig() {
-        // Prepare the platforms for html-markup
-        // - introduces html-table-cell
-        // - adds the info from platform JSON-object
-        // - strips the JSON-identifiers
-        // - adds a checkbox as first table-cell to enable row-selection
-        platformsJSON = configJSON.platforms;
-        platforms = "";
-        for (var id_platform in platformsJSON) {
-            var platform = platformsJSON[id_platform];
-            platforms = platforms + "<tr><td style='vertical-align:middle;'>";
-            platforms = platforms + platform.platform + "</td><td style='vertical-align:middle;'>";
-            platforms = platforms + platform.name + "</td><td style='vertical-align:middle;'>";
-            // FIXME
-            var tempArray = [];
-            for (var element_platform in platform) {
-                tempArray.push(element_platform);
-                tempArray.push(platform[element_platform] + '<br>');
-            }
-            tempArray.splice(tempArray.indexOf('name'), 2);
-            tempArray.splice(tempArray.indexOf('platform'), 2);
-            platforms = platforms + tempArray + "</td>";
-            platforms = platforms + "</td><td style='vertical-align:middle;'><a href='/removePlatform" + id_platform + "' class='btn btn-default center-block' style='width:30px'>-</a>" + "</td><td style='vertical-align:middle;'></td></tr>";
-        }
-
-        // Prepare the accessories for html-markup
-        // - introduces html-table-cell
-        // - adds the info from platform JSON-object
-        // - strips the JSON-identifiers
-        // - adds a checkbox as first table-cell to enable row-selection
-        accessoriesJSON = configJSON.accessories;
-        accessories = "";
-        for (var id_accessory in accessoriesJSON) {
-            var accessory = accessoriesJSON[id_accessory];
-            accessories = accessories + "<tr><td style='vertical-align:middle;'>";
-            accessories = accessories + accessory.accessory + "</td><td style='vertical-align:middle;'>";
-            accessories = accessories + accessory.name + "</td><td style='vertical-align:middle;'>";
-            // FIXME
-            var tempArray = [];
-            for (var element_accessory in accessory) {
-                tempArray.push(element_accessory);
-                tempArray.push(accessory[element_accessory] + '<br>');
-            }
-            tempArray.splice(tempArray.indexOf('name'), 2);
-            tempArray.splice(tempArray.indexOf('accessory'), 2);
-            accessories = accessories + tempArray + "</td>";
-            accessories = accessories + "</td><td style='vertical-align:middle;'><a href='/removePlatform" + id_accessory + "' class='btn btn-default center-block' style='width:135px'>Remove</a>" + "</td><td style='vertical-align:middle;'></td></tr>";
-        }
-
-        // Prepares the html-markup for the bridge parameters as forms
-        bridgeName = "<div class='form-group'><label for='homebridgename'>Name:</label><input type='text' class='form-control' name='bridgeName' value='" + configJSON.bridge.name + "'></div>";
-        bridgeUsername = "<div class='form-group'><label for='username'>Username:</label><input type='text' class='form-control' name='bridgeUsername' value='" + configJSON.bridge.username + "'></div>";
-        bridgePin = "<div class='form-group'><label for='pin'>Pin:</label><input type='text' class='form-control' name='bridgePin' value='" + configJSON.bridge.pin + "'></div>";
+    function decypherText(chunk) {
+        var receivedData = chunk.toString()
+         .replace(/\%7B\%0D\%0A/g,'{')
+         .replace(/\%0D\%0A/g,'')
+         .replace(/\%2C/g,',')
+         .replace(/\%7D/g,'}')
+         .replace(/\%3A/g,':')
+         .replace(/\%22/g,'"')
+         .replace(/\+/g,' ')
+         .replace(/\+\+/g,'')
+         .replace(/\%2F/g,'/')
+         .replace(/\%3C/g,'<')
+         .replace(/\%3E/g,'>')
+         .replace(/\%5B/g,'[')
+         .replace(/\%5D/g,']');
+        return receivedData;
     }
 
     function saveConfig(res) {
         var newConfig = JSON.stringify(configJSON)
-        newConfig = newConfig.replace(',null', '');
-        newConfig = newConfig.replace('null,', '');
-        newConfig = newConfig.replace('null', '');
-        fs.writeFile(process.argv[process.argv.indexOf('-U') + 1] + '/config.json', newConfig, "utf8", savedConfig(res));
+         .replace(/\[,/g, '[')
+         .replace(/,null/g, '')
+         .replace(/null,/g, '')
+         .replace(/null/g, '')
+         .replace(/,,/g, ',')
+         .replace(/,\]/g, ']');
+        newConfig = JSON.stringify(JSON.parse(newConfig), null, 4);
+        fs.writeFile(process.argv[process.argv.indexOf('-U') + 1] + '/config.json', newConfig, "utf8", reloadConfig(res));
     }
 
-    function savedConfig(res) {
-      res.write(header);
-      res.write("<div class='alert alert-danger alert-dismissible fade in out' style='position: absolute; top: 20px; right: 20px;'><a href='/' class='close' data-dismiss='alert'>&times;</a><strong>Note!</strong> Please restart Homebridge to activate your changes.</div>");
-      prepareConfig();
-      printMainPage(res);
+    function reloadConfig(res) {
+        configJSON = require(process.argv[process.argv.indexOf('-U') + 1] + '/config.json');
+        res.write(header);
+        res.write("<div class='alert alert-danger alert-dismissible fade in out'><a href='/' class='close' data-dismiss='alert'>&times;</a><strong>Note!</strong> Please restart Homebridge to activate your changes.</div>");
+        prepareConfig();
+        printMainPage(res);
     }
 
-    function printAddPlatformPage(res) {
+    function prepareConfig() {
+        bridgeName = "<div class='form-group'><label for='homebridgename'>Name:</label><input type='text' class='form-control' name='bridgeName' value='" + configJSON.bridge.name + "'></div>";
+        bridgeUsername = "<div class='form-group'><label for='username'>Username:</label><input type='text' class='form-control' name='bridgeUsername' value='" + configJSON.bridge.username + "'></div>";
+        bridgePin = "<div class='form-group'><label for='pin'>Pin:</label><input type='text' class='form-control' name='bridgePin' value='" + configJSON.bridge.pin + "'></div>";
+
+        platformsJSON = configJSON.platforms;
+        platforms = "";
+        accessoriesJSON = configJSON.accessories;
+        accessories = "";
+
+        const wastebasket = "&#128465";
+        const pen = "&#9997";
+        var symbolToPresent = wastebasket;
+
+        for (var id_platform in platformsJSON) {
+            var platformNoTypeNoName = JSON.parse(JSON.stringify(platformsJSON[id_platform]));
+            delete platformNoTypeNoName.platform;
+            delete platformNoTypeNoName.name;
+            var platform = platformsJSON[id_platform];
+            platforms = platforms + "<tr>"
+             + "<td style='vertical-align:middle;'>" + platform.platform + "</td>"
+              + "<td style='vertical-align:middle;'>" + platform.name + "</td>"
+               + "<td style='vertical-align:middle;'>" + (JSON.stringify(platformNoTypeNoName, null, ' ')).replace(/,/g,',<br>') + "</td>"
+                + "<td style='vertical-align:middle;'><a href='/removePlatform" + id_platform + "' class='btn btn-default center-block' style='height: 34px; line-height: 16px; vertical-align:middle;outline:none !important;'><span style='font-size:25px;''>" + symbolToPresent + ";</span></a>"
+                 + "</td></tr>";
+        }
+
+        for (var id_accessory in accessoriesJSON) {
+            var accessoryNoTypeNoName = JSON.parse(JSON.stringify(accessoriesJSON[id_accessory]));
+            delete accessoryNoTypeNoName.accessory;
+            delete accessoryNoTypeNoName.name;
+            var accessory = accessoriesJSON[id_accessory];
+            accessories = accessories + "<tr>"
+             + "<td style='vertical-align:middle;'>" + accessory.accessory + "</td>"
+              + "<td style='vertical-align:middle;'>" + accessory.name + "</td>"
+               + "<td style='vertical-align:middle;'>" + (JSON.stringify(accessoryNoTypeNoName, null, ' ')).replace(/,/g,',<br>') + "</td>"
+                + "<td style='vertical-align:middle;'><a href='/removeAccessory" + id_accessory + "' class='btn btn-default center-block' style='height: 34px; line-height: 16px; vertical-align:middle;outline:none !important;'><span style='font-size:25px;''>" + symbolToPresent + ";</span></a>"
+                 + "</td></tr>";
+        }
+    }
+
+    function printAddPage(res, type) {
         res.write(header);
         res.write("<div class='container'>");
-        res.write("<body><h2>Add Platform</h2>");
+        res.write("<body><h2>Add " + type + "</h2>");
 
-        res.write("<form enctype='application/x-www-form-urlencoded' action='/savePlatformSettings' method='post'>")
-        res.write("<input type='submit' class='btn btn-default center-block' style='width:135px' value='Add' />");
-        res.write("</form>");
-
+        res.write("<form enctype='application/x-www-form-urlencoded' action='/save" + type + "Settings' method='post'>")
+        res.write("<textarea class='form-control' type='text' name='" + type + "ToAdd' rows='10' placeholder='{ \"" + type  + "\": \"test\" }' required></textarea>");
         res.write("<br>");
+        res.write("<div class='row'>");
+        res.write("<div class='col-xs-offset-1 col-sm-offset-1 col-md-offset-2 col-xs-10 col-sm-9 col-md-8 text-center'>");
+        res.write("<div class='btn-group' data-toggle='buttons'>");
+        res.write("<input type='submit' class='btn btn-default center-block' value='Save' style='width:135px' />");
+        res.write("<a href='/' class='btn btn-default center-block' style='width:135px'>Cancel</a>");
         res.write("</div>");
-        res.end(footer);
-    }
-
-    function printAddAccessoryPage(res) {
-        res.write(header);
-        res.write("<div class='container'>");
-        res.write("<body><h2>Add Accessory</h2>");
-
-        res.write("<form enctype='application/x-www-form-urlencoded' action='/saveAccessorySettings' method='post'>")
-        res.write("<input type='submit' class='btn btn-default center-block' style='width:135px' value='Add' />");
+        res.write("</div>");
         res.write("</form>");
 
         res.write("<br>");
@@ -239,7 +270,7 @@ function Server(log, config) {
         } else {
             res.write("No platforms installed or configured!");
         }
-        res.write("<br><a href='/addPlatform' id='Add' name='AddPlatform' class='btn btn-default center-block' style='width:135px'>New Platform</a><br>");
+        res.write("<a href='/addPlatform' name='AddPlatform' class='btn btn-default center-block' style='width:135px'>Add</a><br>");
 
         res.write("<h2>Accessories</h2>");
         if (0 < Object.keys(accessoriesJSON).length) {
@@ -247,16 +278,13 @@ function Server(log, config) {
         } else {
             res.write("No accessories installed or configured!");
         }
-        res.write("<br><a href='/addAccessory' id='Add' name='AddAccessory' class='btn btn-default center-block' style='width:135px'>New Accessory</a><br>");
+        res.write("<a href='/addAccessory' name='AddAccessory' class='btn btn-default center-block' style='width:135px'>Add</a><br>");
 
-        res.write("<br>");
         res.write("</div>");
         res.end(footer);
     }
 
-    //Lets start our server
     server.listen(self.config.port, function() {
-        //Callback triggered when server is successfully listening. Hurray!
         require('dns').lookup(require('os').hostname(), function(err, add, fam) {
             console.log("[Homebridge-Server] is listening on: http://%s:%s", add, self.config.port);
         })
